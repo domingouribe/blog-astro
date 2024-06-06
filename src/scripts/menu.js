ef595255b225ccb76018158c8bbb02ccbc78b327
Domingo Uribe <uribe.domingo@gmail.com>
2024-06-06 09:42:53 -0400

    Responsive NavBar progress

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,3 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-    document.querySelector(".Nav").classList.toggle("expanded");
+document.querySelector(".Hamburger").addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("isActive");
 });
